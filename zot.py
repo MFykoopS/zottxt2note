5f9f451d82a366b1dc2d528afe0f7e62791f9bbb
MFykoopS <SpookyFM@gmx.de>
2015-05-09 01:30:39 +0200

    adapted zot.py to working with config file

--- zot.py
+++ zot.py
@@ -1,6 +1,8 @@
 from pyzotero import zotero
-# change 'userid' to the libraryid and the API key to the API key from Zotero
-zot = zotero.Zotero('userid', 'user', 'apikey')
+# read userid and apikey from config file
+execfile('zottxt2note.conf')
+
+zot = zotero.Zotero(userid, 'user', apikey)
 articles = zot.items(itemType = 'journalArticle', sort = 'creator', q = 'Jucks')
 print(len(articles))
 
